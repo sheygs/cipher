0e8aa70ee6f128d64bbf14fe9b79931b47367658
sheygs <macbookpro@sheygs-macbook.local>
2024-08-29 20:38:46 +0100

    chore(wip 🏗️): lesson 65

--- main.py
+++ main.py
@@ -2,16 +2,19 @@ text = 'Hello Zaira'
 custom_key = 'python'
 
 def vigenere(message, key):
-  #key_index = 0
+  key_index = 0
   alphabet = 'abcdefghijklmnopqrstuvwxyz'
   count = len(alphabet)
   encrypted_text = ''
 
   for char in message.lower():
-      # append space to the message
+      # Append space to the message
       if char == ' ':
         encrypted_text += char
       else:
+        # Find the right key character to encode
+        key_char=key[key_index % len(key)]
+        key_index += 1
         index = alphabet.find(char)
         new_index = (index + key) % count
         encrypted_text += alphabet[new_index]
